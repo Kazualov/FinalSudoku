aef32a810cc5db6f424266ac974a79789ba2c304
Kazualov <115738058+Kazualov@users.noreply.github.com>
2023-01-29 11:03:05 +0300

    Add files via upload

---
+++ Sudoku/Game.h
@@ -0,0 +1,21 @@
+#include <SFML\Graphics.hpp>
+#include <Windows.h>
+#include <string>
+#include "Sudoku.h"
+
+using namespace std;
+using namespace sf;
+
+class Game
+{
+public:
+	Game(){};
+	void run();
+private:
+	Font font;
+	enum GameState { MENU, GAME, END };
+	GameState state;
+	void levels();
+	void menu();
+};
+
